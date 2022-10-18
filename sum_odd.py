90b2830b3db7a1d7cf5c9586dfe3fc8a0e565820
sobirismatov <name@uzcoder96@gmail.com>
2022-10-18 17:47:05 +0500

    toq sonlari yigindisi

--- sum_odd.py
+++ sum_odd.py
@@ -9,4 +9,5 @@ var_int1=var_int%10
 var_int2=var_int//10%10
 var_int3=var_int//100%10
 var_int4=var_int//1000
-print(var_int1%2*var_int1+var_int2%2*var_int2+var_int3%2*var_int3+var_int4%2*var_int4)
+sum_even=var_int1%2*var_int1+var_int2%2*var_int2+var_int3%2*var_int3+var_int4%2*var_int4
+print(sum_even)
