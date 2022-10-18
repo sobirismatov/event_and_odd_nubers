33822e12a868ef71a06cb7f93445cd7dc0786e20
sobirismatov <name@uzcoder96@gmail.com>
2022-10-18 17:27:31 +0500

    juft raqamlar yigindisi

--- sum_even.py
+++ sum_even.py
@@ -1,12 +1,12 @@
 #A four-digit integer is given. Find the sum of even digits in it.
 
 #Create a variable "var_int" and assign it a four-digit integer value.
-var_int=1234
+var_int=1235
 #Create a variable "sum_even" and assign it 0.
 sum_even=0
 #Find the sum of the even digits in the variable "var_int".
-var_int=1234%10
-var_int1=1234//100
-var_int2=var_int1%10
-sum_even1=var_int+var_int2
-print("1234 sondagi juft raqamlar", var_int, "va", var_int2," juft raqamlar yig`indisi",sum_even1,"ga teng")
+var_int1=var_int%10
+var_int2=var_int//10%10
+var_int3=var_int//100%10
+var_int4=var_int//1000
+print((var_int1+1)%2*var_int1+(var_int2+1)%2*var_int2+(var_int3+1)%2*var_int3+(var_int4+1)%2*var_int4)
--- sum_odd.py
+++ sum_odd.py
@@ -1,12 +1,12 @@
 #A four-digit integer is given. Find the sum of odd digits in it.
 
 #Create a variable "var_int" and assign it a four-digit integer value.
-var_int=1234
+var_int=1235
 #Create a variable "sum_even" and assign it 0.
 sum_even=0
 #Find the sum of the odd digits in the variable "var_int".
-var_int=1234//10
 var_int1=var_int%10
-var_int2=var_int//100
-sum_even1=var_int1+var_int2
-print(sum_even1)
+var_int2=var_int//10%10
+var_int3=var_int//100%10
+var_int4=var_int//1000
+print(var_int1%2*var_int1+var_int2%2*var_int2+var_int3%2*var_int3+var_int4%2*var_int4)
