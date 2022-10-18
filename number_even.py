e97c300763360e3f9aff998753996e29a4418197
sobirismatov <name@uzcoder96@gmail.com>
2022-10-18 17:02:27 +0500

    juft raqamlar soni

--- number_even.py
+++ number_even.py
@@ -1,9 +1,11 @@
 #A four-digit integer is given. Find the number of even digits in it.
 
 #Create a variable "var_int" and assign it a four-digit integer value.
-var_int=1234
+var_int=1333
 #Print the number of even digits in the variable "var_int".
-var_int1=1234%10
-var_int2=1234//100
-var_int3=var_int2%10
-print("1234 sonidagi juft raqamlar" ,var_int1, var_int3)
+var_int1=var_int%10
+var_int2=var_int//10%10
+var_int3=var_int//100%10
+var_int4=var_int//1000
+print((var_int1+1)%2+(var_int2+1)%2+(var_int3+1)%2+(var_int4+1)%2)
+
